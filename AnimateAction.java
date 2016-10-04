cf794885d84c9672c4d4475006fd3a0084b21d37
Trevor Salom <trvrsalom@gmail.com>
2016-10-04 09:02:50 -0700

    buggy

---
+++ AnimateAction.java
@@ -0,0 +1,24 @@
+public class AnimateAction {
+	private int col;
+	private int row;
+	private SpotState state;
+	private int action;
+	private GameState gameState;
+
+	private final int DISPLAY_FULL_COLUMN_ERROR = 0;
+	private final int ANIMATE_CHIP_DROP = 1;
+	private final int DISPLAY_WIN_SCREEN = 2;
+
+	public AnimateAction(int col, int row, SpotState state, GameState gameState) {
+		this.col = col;
+		this.row = row;
+		this.state = state;
+		this.gameState = gameState;
+	}
+
+	public int getCol() { return col; }
+	public int getRow() { return row; }
+	public int getAction() { return action; }
+	public SpotState getSpotState() { return state; }
+	public GameState getGameState() { return gameState; }
+}
